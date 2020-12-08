119f76dbcbc46cfbfb0e9032075ec9a0e790d6ad
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2020-12-08 20:05:56 +0300

    Add Structs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{ post, App, HttpResponse, HttpServer, Responder};
 
-const ANT_HIVE_URL: &str = "localhost:7070";
+const ANT_HIVE_URL: &str = "0.0.0.0:7070";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -16,4 +16,32 @@ async fn main() -> std::io::Result<()> {
 #[post("/")]
 async fn post_async(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
+}
+
+
+struct Ant {
+    pub Id: i32,
+    pub Event: String,
+    pub Errors: u32,
+    pub Age: i32,
+    pub Health: i32,
+    pub Payload: i32,
+    pub Point: Point
+}
+
+struct Point {
+    pub X: i32,
+    pub Y: i32
+}
+
+struct Order {
+    pub AntId: i32,
+    pub Action: String,
+    pub Direction: String
+}
+
+struct Request {
+    pub Id: String,
+    pub Tick: i32,
+    pub Ants: Vec<Ant>
 }
