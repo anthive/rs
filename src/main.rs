8f84fab19dc5fe1a06146b676be76767f840d463
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2020-12-08 19:41:54 +0300

    Add Actix Web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ post, App, HttpResponse, HttpServer, Responder};
+
+const ANT_HIVE_URL: &str = "localhost:7070";
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(post_async)
+    })
+    .bind(ANT_HIVE_URL)?
+    .run()
+    .await
+}
+
+#[post("/")]
+async fn post_async(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
