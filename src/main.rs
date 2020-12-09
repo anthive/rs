af57a06bea9897612b135011dbdbe4c1a6c840ca
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2020-12-09 22:27:46 +0300

    add serialization

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{ post, App, HttpResponse, HttpServer, Responder};
 use serde::{Serialize, Deserialize};
+use serde_json::Result;
 
 const ANT_HIVE_URL: &str = "0.0.0.0:7070";
 const ACTIONS: [&'static str; 5] = ["stay", "move", "eat", "take", "put"];
@@ -18,52 +19,63 @@ async fn main() -> std::io::Result<()> {
 
 #[post("/")]
 async fn post_async(req_body: String) -> impl Responder {
+
     // Here desiarilze
-    //let request = &req_body;
-    let ants: Vec<Ant> = Vec::new();
+    let ants = get_ant_vector(&req_body).unwrap();
 
     let order_from_ants: Vec<Order> = ants.iter()
                             .map(|x| Order {
-                                AntId: x.Id, 
-                                Action: String::from(""), 
-                                Direction: String::from("")})
+                                antId: x.id,
+                                action: String::from(""),
+                                direction: String::from("")})
                             .collect();
 
-    let responce = Responce {Orders: order_from_ants};
-    // Here serialize 
+    let responce = Responce {orders: order_from_ants};
 
-    HttpResponse::Ok().body(req_body)
+    // Here serialize
+    let response_body = serde_json::to_string(&responce).unwrap();
+
+    HttpResponse::Ok().body(response_body)
+}
+
+
+fn get_ant_vector(request: &String ) -> Result<Vec<Ant>> {
+    let ants: Vec<Ant> = serde_json::from_str(&*request.to_string())?;
+    Ok(ants)
 }
 
 #[derive(Serialize, Deserialize)]
 struct Ant {
-    pub Id: i32,
-    pub Event: String,
-    pub Errors: u32,
-    pub Age: i32,
-    pub Health: i32,
-    pub Payload: i32,
-    pub Point: Point
+    pub id: i32,
+    pub event: String,
+    pub errors: u32,
+    pub age: i32,
+    pub health: i32,
+    pub payload: i32,
+    pub point: Point
 }
+
 #[derive(Serialize, Deserialize)]
 struct Point {
-    pub X: i32,
-    pub Y: i32
+    pub x: i32,
+    pub y: i32
 }
+
 #[derive(Serialize, Deserialize)]
 struct Order {
-    pub AntId: i32,
-    pub Action: String,
-    pub Direction: String
+    pub antId: i32,
+    pub action: String,
+    pub direction: String
 }
+
 #[derive(Serialize, Deserialize)]
 struct Request {
-    pub Id: String,
-    pub Tick: i32,
-    pub Ants: Vec<Ant>
+    pub id: String,
+    pub rick: i32,
+    pub ants: Vec<Ant>
 }
 
 #[derive(Serialize, Deserialize)]
 struct Responce {
-    pub Orders: Vec<Order>
+    pub orders: Vec<Order>
 }
